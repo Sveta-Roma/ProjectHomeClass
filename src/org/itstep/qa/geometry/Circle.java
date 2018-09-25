8dfd51fdff410afc6ce19c44470bf354e2a993be
qa <qa.bh@gj>
2018-09-25 18:50:38 +0300

    Default Changelist

--- src/org/itstep/qa/geometry/Circle.java
+++ src/org/itstep/qa/geometry/Circle.java
@@ -6,20 +6,22 @@ public class Circle {
 
     public void setRadius(double in){ //задать радиус
     radius = in;
-    }
+    } // задать радиус
+    //public void setRadius(double radius){//версия препод
+        //this.radius = radius; }
 
     public void getRadius(){
         System.out.println("Радиус круга равен " + radius); //узнать радиус
     }
+    //public double getRadius (){//версия препод
+        //return radius;    }
 
     public double getAreaCircle(){
         double s = pi*radius*radius;
         System.out.println("Площадь круга равна " + s); //узнать площадь
         return s;
     }
+    //public double getAreaCircle(){//версия препод
+        //return Math.PI*radius*radius; }
 
-    /*public void getAreaCircle(){
-        double s = pi*radius*radius;
-        System.out.println("Площадь круга равна " + s); //узнать площадь
-    }*/
 }
--- src/org/itstep/qa/runner/CircleRunner.java
+++ src/org/itstep/qa/runner/CircleRunner.java
@@ -4,11 +4,13 @@ import org.itstep.qa.geometry.Circle;
 
 public class CircleRunner {
     public static void main(String[] args) {
-        Circle circle = new Circle();
-
-        circle.setRadius(2);
-        circle.getRadius();
-        circle.getAreaCircle();
+        Circle circle = new Circle();//версия препод добавить
+        //System.out.println("Было: " + circle.getRadius());версия препод
+        circle.setRadius(2);//версия препод добавить
+        //System.out.println("Стало: " + circle.getRadius());версия препод
+        //System.out.println("Площадь: " + circle.getAreaCircle());версия препод
+        circle.getRadius();//не писать
+        circle.getAreaCircle();//не писать
 
     }
 }
